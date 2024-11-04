3930916a48198f107622f28dad42b3e436e2d595
Niko Niemelä <nokka200@gmail.com>
2024-11-04 12:01:43 +0200

    Task 9.1. valmis

--- calculateBmi.ts
+++ calculateBmi.ts
@@ -1,2 +1,15 @@
 
-console.log("Hello World");
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / (height / 100) ** 2;
+  if (bmi < 18.5) {
+    return "Underweight range";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "Normal range";
+  } else if (bmi >= 25 && bmi < 30) {
+    return "Overweight range";
+  } else {
+    return "Obese range";
+  }
+}
+
+console.log(calculateBmi(180, 74)); 
