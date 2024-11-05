759d4d6007e1ce6559bffdf1b9e5f57593cfb34a
Niku <nokka200@gmail.com>
2024-11-05 15:13:01 +0200

    9.3. bmi valmis

--- calculateBmi.ts
+++ calculateBmi.ts
@@ -1,3 +1,7 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
 
 const calculateBmi = (height: number, weight: number): string => {
   const bmi = weight / (height / 100) ** 2;
@@ -12,4 +16,18 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 }
 
-console.log(calculateBmi(180, 74)); 
+const parseArguments = (args: Array<string>): BmiValues => { 
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const { height, weight } = parseArguments(process.argv);
+console.log(calculateBmi(height, weight)); 
